0b643289ae3cbbd66e42dd11cc04fa73990e220d
Joshua Mabry <inkandcapture@gmail.com>
2016-09-27 16:44:57 -0400

    Completed Programming, Working on Append Data

--- tipCalculator.js
+++ tipCalculator.js
@@ -1,10 +1,52 @@
-// Calculate Tip Amount
+var tipAmt = 0;
+var pplSharing = 0;
+var billTotalAmt = 0;
+var billAmt = 0;
 
-// Divid Total Bill by Amount of People Sharing
+
+// Calculate Tip Amount
+function calculateTip () {
+    billAmt = $("#billCost").val();
+    console.log(billAmt);
+    if( $("#service").val() == $("#outstanding").val()) {
+        tipAmt = billAmt * .30;
+        billDivide(tipAmt);
+    } else if ( $("#service").val() == $("#good").val()) {
+        tipAmt = billAmt * .20;
+        billDivide(tipAmt);
+    } else if ( $("#service").val() == $("#okay").val()) {
+        tipAmt = billAmt * .15;
+        billDivide(tipAmt);
+    } else if ( $("#service").val() == $("#bad").val()) {
+        tipAmt = billAmt * .10;
+        billDivide(tipAmt);
+    }else if ( $("#service").val() == $("#terrible").val()) {
+        tipAmt = billAmt * .05;
+        billDivide(tipAmt);
+    }
+        console.log("Didn't make a service choice!")
+}
+
+
+// Divide Total Bill by Amount of People Sharing
+function billDivide(calculateTip){
+    console.log("Made it to billDivide");
+    pplSharing = $("#peopleSharing").val();
+
+    billTotalPp = billAmt / pplSharing;
+    tipAmt = tipAmt / pplSharing;
+    billTotalAmt = billTotalPp + tipAmt;
+
+    console.log(tipAmt);
+    console.log(billTotalPp);
+    console.log(billTotalAmt);
+
+ 
+}
 
 // Show Tip Amount and Total Bill
 
 
-$(function) {
-    // Submit user input for bill amount
+$(function(){
+    $("#calculate").on("click", calculateTip);
 });
